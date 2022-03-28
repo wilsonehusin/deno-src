5860d7f851808f3935d50cbc33f6ba7ef427e1f6
Wilson E. Husin <wilson@husin.dev>
2022-03-27 18:25:54 -0700

    httpdump: add path, upfront env gathering

--- httpdump/httpdump.ts
+++ httpdump/httpdump.ts
@@ -1,38 +1,39 @@
 import { serve } from "std/http/server.ts";
 import { VERSION } from "std/version.ts";
 
-const defaultHeaders: Record<string, string> = {
+const headers: Record<string, string> = {
   "content-type": "application/json",
-  "x-deno-std": VERSION,
 };
+
+if (Deno.version !== undefined) {
+  headers["x-deno"] = Deno.version.deno;
+  headers["x-deno-v8"] = Deno.version.v8;
+  headers["x-deno-ts"] = Deno.version.typescript;
+}
+
+headers["x-deno-std"] = VERSION
+
+const region = Deno.env.get("DENO_REGION")
+if (region) {
+  headers["x-deno-region"] = region
+}
 
 async function handler(request: Request): Promise<Response> {
   const reqHeaders: Record<string, string> = {};
   request.headers.forEach((v, k) => {
     reqHeaders[k] = v;
   });
+  const url = new URL(request.url);
   const req = {
     method: request.method,
-    url: request.url,
+    url: url,
+    path: url.pathname,
     credentials: request.credentials,
     mode: request.mode,
     headers: reqHeaders,
     redirect: request.redirect,
     body: await request.text(),
   };
-
-  const headers = defaultHeaders;
-
-  if (Deno.version !== undefined) {
-    headers["x-deno"] = Deno.version.deno;
-    headers["x-deno-v8"] = Deno.version.v8;
-    headers["x-deno-ts"] = Deno.version.typescript;
-  }
-
-  const region = Deno.env.get("DENO_REGION");
-  if (region != undefined && region != "") {
-    headers["x-deno-region"] = region;
-  }
 
   return new Response(JSON.stringify(req) + "\n", { headers });
 }
